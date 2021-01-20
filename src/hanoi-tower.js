02311a59f3aa3bf4d320dd26620dfc91ec245cd0
Hotja <dudechka@mail.ru>
2021-01-20 23:08:05 +0300

    аново fork, переделываю задания изменившееся

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,5 +2,5 @@ module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = turns / (turnsSpeed / 3600);
 
-  return {turns: turns, seconds: seconds};
+  return {turns: turns, seconds: Math.trunc(seconds)};
 }
