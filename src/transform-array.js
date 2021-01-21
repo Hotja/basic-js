57b03ad0b89f1e39f08243f2ba2dee74bf489e36
Hotja <dudechka@gmail.com>
2021-01-21 14:25:05 +0300

    transArray fix error

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,24 +3,29 @@ module.exports = function transform(arr) {
   if (!Array.isArray(arr)) throw new Error('not an array');
   let NewArr = [];       
 
-for (let i = 0; i < arr.length; i++) {
-  switch (arr[i]) {
-    case '--discard-next':
-      i=i+2;
-      break;
-    case '--discard-prev':
-      if (NewArr.length != 0) NewArr.pop()
-      break;
-    case '--double-next':
-      if (i < arr.length - 1) NewArr.push(arr[i + 1]);
-      break;
-    case '--double-prev':
-      if (i >= 1) NewArr.push(arr[i - 1]);
-      break;
-    default:
-      NewArr.push(arr[i]);
+  for (let i = 0; i < arr.length; i++){
+    if (typeof arr[i] == 'string'){
+      switch(arr[i]){
+        case '--discard-next': 
+        if (i == arr.length - 1) break;
+        i++; break;
+        case '--discard-prev': 
+        if (i == 0) break;
+        if (arr[i - 2] != '--discard-next'){
+          NewArr.pop(); 
+        }break;
+        case '--double-next': 
+        if (i == arr.length - 1) break;
+        NewArr.push(arr[i+1]); break;
+        case '--double-prev': 
+        if (i == 0) break;
+        if (arr[i - 2] != '--discard-next'){
+          NewArr.push(arr[i-1]); 
+        }break;
+        default: NewArr.push(arr[i]); break;
+      }
+    } else NewArr.push(arr[i]);
   }
-}
-return NewArr;
+  return NewArr;  
 
 };
